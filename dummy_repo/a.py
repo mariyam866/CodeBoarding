905891d250eaa841836deacccfc70ae4b95929b5
brovatten <alexander_bridgwater@live.se>
2025-04-09 18:54:54 +0200

    dummy repo

---
+++ dummy_repo/a.py
@@ -0,0 +1,4 @@
+# class_c.py
+class C:
+    def save(self, data):
+        print(f"[C] Saving data: {data}")
---
+++ dummy_repo/b.py
@@ -0,0 +1,6 @@
+# class_b.py
+class B:
+    def process(self, input_data):
+        processed = input_data.upper()
+        print(f"[B] Processed data: {processed}")
+        return processed
